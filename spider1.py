64baf8e37fbcb238a1f837d9924601d230ae44c1
Wang Caiqi <tjuwang2013@163.com>
2017-01-03 12:10:23 +0800

    add user-agent

--- spider1.py
+++ spider1.py
@@ -1,9 +1,11 @@
 import urllib2
 def download(url,num_retries=2):
     print 'Downloading:' ,url 
+    headers = {'User-agent':'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322)'}
+    request = urllib2.Request(url,headers=headers
     try:
 
-        html = urllib2.urllib2(url).read()
+        html = urllib2.urllib2(request).read()
     except urllib2 URLError as e:
         print 'Download',e.reason
         html = None
